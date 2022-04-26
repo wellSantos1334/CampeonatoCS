ca35b37bf977e5678aeccc0c83c92821bb9cf42f
well <44660239+wellzin134@users.noreply.github.com>
2022-04-26 19:52:23 -0300

    fix criarTorneio.php

--- php/criarTorneio.php
+++ php/criarTorneio.php
@@ -9,10 +9,11 @@ $dataInicio = $_GET['dataInicio'];
 $sql = "INSERT INTO torneio (nome, premiacao, data) VALUES ('$nomeTorneio', '$premiacao', '$dataInicio')";
 
 if(mysqli_query($conexao, $sql)){
-    echo "Inserido no BD";
+    echo "<script> alert('Torneio criado com sucesso!')</script>";
+    require_once('../criarTorneio.php');
 }else {
     echo "Erro".mysqli_error($conexao);
 }
 
-
 ?>
+
