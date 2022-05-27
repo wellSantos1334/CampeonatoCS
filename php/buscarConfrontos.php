b843d91c4a4e43da5dfaebc00ba7c401f5579b67
wellSantos1334 <103972693+wellSantos1334@users.noreply.github.com>
2022-05-26 23:58:38 -0300

    fix buscarConfrontos

--- php/buscarConfrontos.php
+++ php/buscarConfrontos.php
@@ -26,37 +26,73 @@ $idTorneio = $_GET['idTorneio'];
 
     <section>
         <h3>Confrontos</h3>
-
-
         <?php
-        $sql = "SELECT * FROM partida p JOIN serie s on p.codTorneio = '$idTorneio'";
+        $sql = "SELECT * FROM partida WHERE codTorneio = '$idTorneio'";
         $query = mysqli_query($conexao, $sql) or die("Erro ao conectar com o banco de dados! " . mysqli_errno($conexao));
+        echo $sql;
         while ($dados = $query->fetch_assoc()) {
         ?>
             <table class="table-confronto">
                 <tr>
-                    <th class="th-categoria" colspan="3">GrandFinal</th>
+                    <th class="th-categoria" colspan="3"><?php echo $dados['categoria'] ?></th>
                 </tr>
                 <tr>
 
                     <th class="th-mapa" colspan="3"><?php echo $dados['mapa'];?></th>
                 </tr>
                 <tr>
-                    <td class="td-times">Time1</td>
-                    <td class="td-reshalf1">5</td>
-                    <td class="td-reshalf2">13</td>
+                    <td class="td-times">
+                        <?php 
+                            $codTime1 = $dados['codTime1'];
+                            $sqlTime1 = "SELECT * FROM times WHERE id = '$codTime1'";
+                            $queryTime1 = mysqli_query($conexao, $sqlTime1) or die("Erro ao conectar com o banco de dados! " . mysqli_errno($conexao)); 
+                            $dadosTime1 = $queryTime1->fetch_assoc();
+                            echo $dadosTime1['nome'];
+                        ?>
+                    </td>
+                    
+                    <td class="td-reshalf1" id="teste1">
+                        <?php echo $dados['half1Time1'];?>
+                    </td>
+                    <td class="td-reshalf2">
+                        <?php echo $dados['half2Time1'];?>
+                    </td>
                 </tr>
                 <tr>
-                    <td class="td-times">Time2</td>
-                    <td class="td-reshalf1">10</td>
-                    <td class="td-reshalf2">16</td>
+                    <td class="td-times">
+                        <?php 
+                            $codTime2 = $dados['codTime2'];
+                            $sqlTime2 = "SELECT * FROM times WHERE id = '$codTime2'";
+                            $queryTime2 = mysqli_query($conexao, $sqlTime2) or die("Erro ao conectar com o banco de dados! " . mysqli_errno($conexao)); 
+                            $dadosTime2 = $queryTime2->fetch_assoc();
+                            echo $dadosTime2['nome'];
+                        ?> 
+                    </td>
+                    <td class="td-reshalf1" id="teste2">
+                        <?php echo $dados['half1Time2'];?>
+                    </td>
+                    <td class="td-reshalf2">
+                        <?php echo $dados['half2Time2'];?>
+                    </td>
                 </tr>
             </table>
         <?php
         }
         ?>
     </section>
+        <!-- <script>
+            var teste1 = document.querySelector(".th-categoria");
+            var teste2 = document.querySelector("#teste2");
 
+            teste1.style.backgroundColor = "red";
+
+            // console.log(teste1);
+
+            // if(teste1 > teste2){
+            //     teste2.backgroundColor = "blue";
+            // }
+
+        </script> -->
 </body>
 
 </html>
